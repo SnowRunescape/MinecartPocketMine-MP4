6a5a76d08e14e10e1b77fe4b9cc9efef9262a250
Bruno Caitano <snowrunescape@snowdev.com.br>
2022-11-03 20:56:14 -0400

    feat: base para pegar as entregas pendentes

--- src/Minecart/AutomaticDelivery.php
+++ src/Minecart/AutomaticDelivery.php
@@ -2,6 +2,8 @@
 
 namespace Minecart;
 
+use Minecart\utils\API;
+
 class AutomaticDelivery
 {
     const NONE = 0;
@@ -9,6 +11,17 @@ class AutomaticDelivery
     const ANYTIME = 2;
 
     public function run(): void
+    {
+        $api = new API();
+        $api->setAuthorization($this->authorization);
+        $api->setShopServer($this->shopServer);
+        $api->setParams(["username" => $this->username]);
+        $api->setURL(API::DELIVERY_PENDING_URI);
+
+        $this->setResult($api->send());
+    }
+
+    public function onCompletion(): void
     {
         // TODO
     }
